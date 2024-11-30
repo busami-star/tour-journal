f9380406d8f357b1c35cfebd3e7ab7f033745a4b
busami-star <victorbusami1@gmail.com>
2024-12-01 02:17:46 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import Header from './components/Header.jsx'
 import Entry from './components/Entry.jsx'
+import data from './components/data.js'
 
 
 function App() {
+
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry 
+            img = {entry.img}
+            title={entry.title}
+            country={entry.country}
+            googleMapsLink={entry.googleMapsLink}
+            dates={entry.dates}
+            text={entry.text}
+            />
+        )
+    })
   return (
     <>
       <Header />
-      <Entry />    
-      
-      
+      <main>
+        {entryElements}
+      </main> 
     </>
   );
 }
---
+++ src/components/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://imgs.search.brave.com/6VeEZpdLbbvVlsr556gA5zzM-Ni-Gjdcax4aqWzOoMw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZi5i/c3RhdGljLmNvbS94/ZGF0YS9pbWFnZXMv/aG90ZWwvbWF4MTAy/NHg3NjgvNDEyNDQ2/MTU4LmpwZz9rPWQw/NWM3ZGYxM2UxMjRm/ZGViZjc2MjcyNTFh/OTAyOWExNzAzNmY2/YjQyZGNiZmQ0YjNk/MjIwOGYwMTE0ZTU2/NmYmbz0maHA9MQ",
+            alt: "Imperial Hotel"
+        },
+        country: "Kisumu",
+        title: "Imperial hotel",        
+        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+        dates: "12 august, 2024",
+        text: "The hotel offers a comfortable and clean environment, with many reviewers praising the beds and room quality."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://imgs.search.brave.com/-qPRNdG83nPRRnaN3JlPDpfgCZZ1sDI1cuzP0hSMWQY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Zml2ZXN0YXJhbGxp/YW5jZS5jb20vZmls/ZXMvZml2ZXN0YXJh/bGxpYW5jZS5jb20v/ZmllbGQvaW1hZ2Uv/bm9kZXMvMjAxNS8y/NDY4OC8yNDY4OF8w/X3ZpbGxhcm9zYWtl/bXBpbnNraW5haXJv/YmlfZnNhLWcuanBn",
+            alt: "villa rosa"
+        },
+        title: "Kempinski Villa Rosa",
+        country: "Nairobi",
+        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+        dates: "27 September",
+        text: "The hotel's multiple restaurants offer diverse and high-quality cuisine, ranging from Italian to Middle Eastern and Chinese, making it a standout feature for many visitors"
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://imgs.search.brave.com/SiW08bT4Amu7ZHIBwPwjor54i2TMUJRFhsxLAS-Rh24/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTE0/MzQ0MjUxL3Bob3Rv/L3N0YXR1ZS1pbi1m/cm9udC1vZi1uYWly/b2JpLW5hdGlvbmFs/LW11c2V1bS5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9UGFG/RUVBVmhpbmJsTEZY/TVF5cUp3RE9rQjlL/Yy0tQlllQmwyclZw/T0lFZz0",
+            alt: "museum"
+        },
+        title: "Nairobi National Museum",
+        country: "Nairobi",
+        googleMapsLink: "https://www.google.com/maps/place/Nairobi+National+Park/@-1.3624305,36.8376042,12z",
+        dates: "01 Oct, 2024",
+        text: "The Nairobi National Museum is highly praised for showcasing Kenya's rich heritage through exhibits on culture, nature, history, and contemporary art. Visitors often highlight the extensive collection of prehistoric artifacts, including fossils and displays about Kenya's diverse wildlife and bird species. The museum's location on Museum Hill, along with its adjacent Snake Park and botanical gardens, adds to its appeal. Many also commend the educational value of the exhibits and the well-maintained grounds, making it a favorite cultural spot in Nairobi​."
+    },
+]
